407ba1880e8c5b657c3fde52a37638b0fc5903e6
AcessPlus <contact.acessplus@gmail.com>
2025-07-29 17:53:32 -0300

    projeto novo

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        garet: ['Garet-Book' , 'sans-serif'],
+        'garet-bold':['Garet-Heavy' , 'sans-serif']
+
+      }
+
+    },
+  },
+  plugins: [],
+}
+
